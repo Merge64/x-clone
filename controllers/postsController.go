ea316c0457c36ca7bf8460cb7be23b32fe49efd9
Santiago Piccoli <121751727+spiccoli@users.noreply.github.com>
2025-01-09 20:33:47 -0300

    Update postsController.go

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -41,8 +41,8 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 
 	var parentID, quoteID *uint
 	if parentIDStr != "" {
-		parsedParentID, err2 := strconv.ParseUint(parentIDStr, 10, 32)
-		if err2 != nil {
+		parsedParentID, ParentErr := strconv.ParseUint(parentIDStr, 10, 32)
+		if ParentErr != nil {
 			http.Error(w, "Invalid parent ID", http.StatusBadRequest)
 			return
 		}
@@ -51,8 +51,8 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 	}
 
 	if quoteIDStr != "" {
-		parsedQuoteID, err3 := strconv.ParseUint(quoteIDStr, 10, 32)
-		if err3 != nil {
+		parsedQuoteID, ParsedErr := strconv.ParseUint(quoteIDStr, 10, 32)
+		if ParsedErr != nil {
 			http.Error(w, "Invalid quote ID", http.StatusBadRequest)
 			return
 		}
